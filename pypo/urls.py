6824f85bfffbcd1be3d20cea1a6408ed719095b4
Jens Kadenbach <kadenbach@eventit.local>
2013-08-16 00:06:23 +0200

    Generic views for adding and listing items

--- pypo/urls.py
+++ pypo/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from readme import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,9 +12,14 @@ urlpatterns = patterns('',
     # url(r'^$', 'pypo.views.home', name='home'),
     # url(r'^pypo/', include('pypo.foo.urls')),
 
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^add/$', views.AddView.as_view(), name='index'),
+    url(r'^view/(?P<pk>\d+)$', views.ItemView.as_view(), name='index'),
+
+
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
--- readme/views.py
+++ readme/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import generic
+from .models import Item
+
+
+class IndexView(generic.ListView):
+    context_object_name = 'current_item_list'
+
+    def get_queryset(self):
+        return Item.objects.order_by('-created')
+
+
+class AddView(generic.CreateView):
+    model = Item
+    success_url = "/"
+
+class ItemView(generic.DetailView):
+    model = Item
+    
+        
