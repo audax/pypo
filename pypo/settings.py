25f8865c59b0d54a2fe42659a15b48614d307ff5
Jens Kadenbach <kadenbach@eventit.local>
2013-08-15 20:52:58 +0200

    django-crispy-forms installed

--- pypo/settings.py
+++ pypo/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'readme',
+    'crispy_forms',
 )
 
 # A sample logging configuration. The only tangible logging
