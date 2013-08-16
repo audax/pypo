c49e14bda89b4436e21f76e1f9ba7c37dd494be2
Jens Kadenbach <kadenbach@eventit.local>
2013-08-16 16:36:53 +0200

    Scrape the title and a summary from submitted links
    migration for the readable artice field

--- readme/models.py
+++ readme/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.html import strip_tags
 
 
 class Item(models.Model):
@@ -7,4 +8,10 @@ class Item(models.Model):
     title = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User)
+
+    readable_article = models.TextField(null=True)
+
+    @property
+    def summary(self):
+        return strip_tags(self.readable_article)[:300]
 
--- readme/views.py
+++ readme/views.py
@@ -2,13 +2,18 @@ from django.views import generic
 from .models import Item
 from .forms import CreateItemForm
 from django.http import HttpResponseRedirect
-from django.contrib.auth.models import User
+from readability import Document
+import requests
+import re
 
 class IndexView(generic.ListView):
     context_object_name = 'current_item_list'
 
     def get_queryset(self):
         return Item.objects.order_by('-created')
+
+class DeleteItem(generic.DeleteView):
+    model = Item
 
 
 class AddView(generic.CreateView):
@@ -19,7 +24,17 @@ class AddView(generic.CreateView):
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
+        try:
+            self.object = self.model.objects.get(url=self.object.url, owner=self.request.user)
+        except Item.DoesNotExist:
+            pass
+
         self.object.owner = self.request.user
+        req = requests.get(self.object.url)
+        doc = Document(req.text)
+        self.object.title = doc.short_title()
+        self.object.readable_article = doc.summary(True)
+
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
 
