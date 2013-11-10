aca2e2f046a25a6bd5ffeb6db2610b06aa29e611
Jens Kadenbach <kadenbach@eventit.local>
2013-11-10 22:32:51 +0100

    Changed project structure
    This project should now be in a folder called "source" inside
    the project folder.

--- pypo/settings.py
+++ pypo/settings.py
@@ -1,4 +1,6 @@
 import os
+from os import path
+PROJECT_ROOT = path.abspath(path.join(path.dirname(__file__), '..'))
 
 # Django settings for pypo project.
 
@@ -63,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = path.join(PROJECT_ROOT, '../static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
