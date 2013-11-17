eebb7873826b477bd77dace180bc8921f71e42ab
Jens Kadenbach <kadenbach@eventit.local>
2013-11-17 15:52:41 +0100

    More robust qunit runner

--- qunit.sh
+++ qunit.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 wget https://raw.github.com/jquery/qunit/master/addons/phantomjs/runner.js
 ./manage.py runserver &
+SERVER=$!
 sleep 5
 phantomjs runner.js http://localhost:8000/static/tests/setup.html
 SUCCESS=$?
-kill %1
+kill $SERVER
 rm runner.js
 exit $SUCCESS
