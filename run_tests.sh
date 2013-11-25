8e528badbb4a36d2061a8a0923f38d48d04411f1
Jens Kadenbach <kadenbach@eventit.local>
2013-11-25 22:22:23 +0100

    Testrunner

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 wget https://raw.github.com/jquery/qunit/master/addons/phantomjs/runner.js
-./manage.py runserver &
+python manage.py runserver &
 SERVER=$!
-sleep 5
+sleep 3
 phantomjs runner.js http://localhost:8000/static/tests/setup.html
 SUCCESS=$?
 kill $SERVER
 rm runner.js
-exit $SUCCESS
+if python manage.py test; then
+    exit $SUCCESS
+fi
+exit 1
