32241d553d0dbb71d94e4efe17199f480ad7da9b
Jens Kadenbach <kadenbach@eventit.local>
2013-11-25 20:57:44 +0100

    Travis ci config

--- pypo/settings.py
+++ pypo/settings.py
@@ -20,8 +20,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'pypo',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'pypo',
-        'PASSWORD': 'pypo',
+        'USER': 'postgres',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
